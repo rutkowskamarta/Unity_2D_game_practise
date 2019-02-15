8d810c61aeabc8d7ade8e8d1e6546f16a8c52504
Marta Rutkowska <marta.rutkowska@protonmail.com>
2019-02-15 18:23:26 +0100

    player movement added

---
+++ 2D_game_practise/Assets/Scripts/CursorControllerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorControllerScript : MonoBehaviour {
+
+    [SerializeField] private float smoothDelay = 0.1f;
+    [SerializeField] private float playersDelay = 3f;
+    [SerializeField] private GameObject playersSprite;
+    [SerializeField] private GameObject cursorGameObject;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        RotatePlayerTowardsMousePositon();	
+	}
+
+    private void RotatePlayerTowardsMousePositon()
+    {
+        Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = (mouse - (Vector2)transform.position).normalized;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        cursorGameObject.transform.rotation = Quaternion.Euler(cursorGameObject.transform.eulerAngles.x, cursorGameObject.transform.eulerAngles.y, angle-90);
+        playersSprite.transform.rotation = Quaternion.Slerp(playersSprite.transform.rotation, cursorGameObject.transform.rotation, Time.deltaTime * playersDelay);
+    }
+}
---
+++ 2D_game_practise/Assets/Scripts/PlayerControllerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerScript : MonoBehaviour {
+    [SerializeField] private Transform cursorObject;
+    [SerializeField] private float playerSpeed = 10;
+
+    private Rigidbody2D playerRigidbody;
+    
+	void Start () {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+        Move();	
+	}
+
+    private void Move()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 direction = cursorObject.position - transform.position;
+            playerRigidbody.velocity = direction*playerSpeed*Time.deltaTime;
+        }
+        else
+        {
+            playerRigidbody.velocity = Vector2.zero;
+        }
+    }
+}
